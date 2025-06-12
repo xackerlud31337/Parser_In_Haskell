61355d3cbae7058d8167ed13e385db39a26e8dbc
xackerlud31337 <i.mandev@student.utwente.nl>
2025-06-12 16:58:51 +0200

    fixed the comments

--- BasicParsers.hs
+++ BasicParsers.hs
@@ -19,7 +19,7 @@ letter = Parser $ \inp ->
         Stream (c:cs) | isAlpha c -> [(c, Stream cs)]
         _ -> []
 
---FP2.1 by <Ivan Mandev>
+--by <Ivan Mandev>
 --Parses any single digit character
 dig :: Parser Char
 dig = Parser $ \inp -> 
@@ -36,17 +36,17 @@ space = Parser $ \inp ->
         Stream (c:cs) | isSpace c -> [(c, Stream cs)]
         _ -> []
 
---FP2.2 by <Ivan Mandev>
+--by <Ivan Mandev>
 --Parser that consumes zero or more whitespace characters
 skipSpaces :: Parser [Char]
 skipSpaces = many space
 
---FP2.2 by <Ivan Mandev>
+--by <Ivan Mandev>
 --Runs the parsers in sequence
 between :: Parser a -> Parser b -> Parser c -> Parser b
 between p1 p2 p3 = p1 *> p2 <* p3
 
---FP2.2 by <Ivan Mandev>
+--by <Ivan Mandev>
 -- Wraps parser p by skipping whitespace before and after
 whitespace :: Parser a -> Parser a
 whitespace p = skipSpaces *> p <* skipSpaces
@@ -56,12 +56,12 @@ whitespace p = skipSpaces *> p <* skipSpaces
 sep1 :: Parser a -> Parser b -> Parser [a]
 sep1 p s = (:) <$> p <*> many (s *> p)
 
---FP2.3 by <Ivan Mandev>
+--by <Ivan Mandev>
 -- sep p s parses zero or more occurrences of p, separated by s
 sep :: Parser a -> Parser b -> Parser [a]
 sep p s = sep1 p s <|> pure []
 
---FP2.3 by <Ivan Mandev>
+--by <Ivan Mandev>
 -- option x p tries to run p; if p fails, returns x without consuming input
 option :: a -> Parser a -> Parser a
 option x p = p <|> pure x
@@ -71,29 +71,29 @@ string :: String -> Parser String
 string [] = pure []
 string (c:cs) = (:) <$> char c <*> fmap (c:) (string cs)
 
---FP2.4 by <Ivan Mandev>
+--by <Ivan Mandev>
 -- Parses an identifier surrounded by optional whitespace
 identifier :: Parser String
 identifier =
     whitespace ((:) <$> letter <*> many (letter <|> dig))
 
---FP2.4 by <Ivan Mandev>
+--by <Ivan Mandev>
 integerBare :: Parser Integer
 integerBare =
   fmap read $ (++) <$> option "" (string "-") <*> some dig 
 
---FP2.4 by <Ivan Mandev>
+--by <Ivan Mandev>
 integer :: Parser Integer
 integer = whitespace integerBare
 
---FP2.4 by <Ivan Mandev>
+--by <Ivan Mandev>
 symbol :: String -> Parser String
 symbol lit = whitespace (string lit)
 
---FP2.4 by <Ivan Mandev>
+--by <Ivan Mandev>
 parens :: Parser a -> Parser a
 parens p = symbol "(" *> p <* symbol ")"
 
---FP2.4 by <Ivan Mandev>
+--by <Ivan Mandev>
 braces :: Parser a -> Parser a
 braces p = symbol "{" *> p <* symbol "}"
